6219131fd8339fbb6b25353ef837448b181806fd
Ayoazeez26 <ayoazeez26@gmail.com>
2022-09-18 22:55:32 +0100

    added faq

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -5,10 +5,13 @@ export default {
 
 <template>
   <footer id="footer" class="bg-black mx-auto mt-5">
-    <div class="container-gravy flex flex-col-reverse md:flex-row justify-between md:items-center footer relative mx-auto rounded-lg px-10 py-7">
+    <div class="container-gravy flex flex-col-reverse md:flex-row justify-between items-center footer relative mx-auto rounded-lg px-10 py-7">
       <p class="text-xs copy text-white">
         © 2022 Rewind Inc. All rights reserved.
       </p>
+      <a href="https://splish.notion.site/REWIND-FAQ-a4b30fcde63d43cd9185ff9edb3a4c1f" target="_blank" class="text-white mb-3 md:mb-0">
+        FAQs
+      </a>
       <div class="flex justify-between items-center max-w-min">
         <a href="https://www.facebook.com" target="_blank" class="item w-10 h-10 flex items-center justify-center">
           <img src="@/assets/icons/facebook.svg" alt="Facebook Icon" class="">
