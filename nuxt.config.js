aaefe6ec7d24bf280f503ae3f66e88de30184aae
Abdulhakeem Abdulazeez Ayodeji <44169294+Ayoazeez26@users.noreply.github.com>
2022-12-08 15:00:54 +0100

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,14 +5,14 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Rewind',
+    title: 'REWIND',
     htmlAttrs: {
       lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'Helping businesses issue refunds faster and more efficiently.' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
